d434b6c70fbbc253dbfacdb80ef6fac7e8ec700e
Jerlo F. De Leon <cresentyt@gmail.com>
2026-07-24 23:22:04 +0800

    feat(tests): add playback view button tests for queue, previous, pause/resume, next, and repeat functionalities

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,6 +26,23 @@ def mock_track():
 
 
 @pytest.fixture
+def mock_get_tracks():
+    """
+    Returns a list of tuples (uri, title, thumbnail, length)
+    This is from the `get_tracks` function at core/utils.py
+    Usage:
+    - mock_get_tracks(mock_player)
+
+    Returns:
+    - list[tuple[str, str, str, int]]
+    """
+    return [
+        ("https://link1.com", "Song Title 1", "thumbnail1", 180),
+        ("https://link2.com", "Song Title 2", "thumbnail2", 210),
+    ]
+
+
+@pytest.fixture
 def mock_user():
     user = MagicMock(spec=nextcord.User)
     user.id = 12345
